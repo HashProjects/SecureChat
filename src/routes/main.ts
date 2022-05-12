a48e95fbf0cf9a47e08ab1868fded1b7a03667fa
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-11 21:42:26 -0700

    Added login and register endpoints to router

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,12 +1,12 @@
-import { chat, home } from "../handlers/pages";
+import { chat, home, login, register } from "../handlers/pages";
 import express, { Router } from "express";
-import logging from "../config/logging";
-import { Request, Response, NextFunction } from "express";
 
 const router = Router();
 router.use(express.static("public"));
 
 router.get("/", home);
 router.get("/chat", chat);
+router.get("/login", login);
+router.get("/register", register);
 
 export default router;
