36503d35869b6bb1b3f3099a34b0106832e36397
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-12 00:30:00 -0700

    Beginning to implement jwt logic

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -3,6 +3,9 @@ import logging from "../config/logging";
 import { query } from "../helpers/db";
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+import { readFileSync } from "fs";
 
 const NAMESPACE = "auth";
 
@@ -14,33 +17,63 @@ type authInfo = {
 };
 
 /**
+ * Warning if Default JWT Key is being used
+ */
+if (config.jwt.private.includes("default")) {
+  logging.warn(
+    NAMESPACE,
+    "Default JWT Key used. This is unsafe for production. Only use for development. Refer to the README for key generation"
+  );
+}
+
+const PRIVATE_KEY = readFileSync(config.jwt.private);
+const PUBLIC_KEY = readFileSync(config.jwt.public);
+
+/**
  * The handler for user registration.
  *
  * { username: string, password: string }
  */
 export const register = async (req: Request, res: Response) => {
   /*  Input validation */
-  if (!req.body) return res.status(400).json({ message: "missing request body" });
-  if (!req.body.username?.length) return res.status(400).json({ message: "Invalid Username" });
-  if (!req.body.password?.length) return res.status(400).json({ message: "Invalid Password" });
+  if (!Object.keys(req.body).length)
+    return res.status(400).json({ message: "Missing request body" });
+  if (!req.body.username?.length) return res.status(400).json({ message: "Invalid username" });
+  if (!req.body.password?.length) return res.status(400).json({ message: "Invalid password" });
   const { username, password }: authInfo = req.body;
 
+  /* Generate user object and uuidv4 */
   const user = new User(username);
 
-  const hashedPassword = bcrypt.hash(password, SALT_ROUNDS);
+  /* Hash and Salt Password */
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-  logging.debug(NAMESPACE, `${user.name}, ${user.id}, ${hashedPassword}`);
-
-  await query("INSERT INTO Users (username, id, password) VALUES (?, ?, ?)", [
+  /* Insert into DB */
+  query("INSERT INTO Users (username, id, password) VALUES (?, ?, ?)", [
     user.name,
     user.id,
     hashedPassword,
-  ]).catch((err: Error) => {
-    logging.error(NAMESPACE, err.toString());
-    return res.status(406).json({
-      message: "This username has already been taken",
+  ])
+    /* If Successful */
+    .then(() => {
+      res.redirect("/chat");
+    })
+    /* If Error */
+    .catch((err) => {
+      /* Duplicate Username */
+      if (err.code === "SQLITE_CONSTRAINT") {
+        res.status(406).json({
+          message: "This username has already been taken",
+        });
+      } else {
+        /* Database failure */
+        logging.error(NAMESPACE, err);
+        res.status(500).json({
+          message: "Database Error",
+          error: err,
+        });
+      }
     });
-  });
 };
 
 /**
