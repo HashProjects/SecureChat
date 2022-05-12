c7f49786af24a022cef92c0a054a99894a0876ae
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-11 22:00:17 -0700

    added input validation for auth handlers

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
 import { query } from "../helpers/db";
 
+type authInfo = {
+  username: string,
+  password: string
+}
+
+/**
+ * The handler for user registration.
+ * 
+ * { username: string, password: string }
+ */
 export const register = (req: Request, res: Response) => {
-  res.send("Register");
+  /*  Input validation */
+  if (!req.body) return res.status(400).json({ message: "missing request body" });
+  if (!req.body.username?.length) return res.status(400).json({ message: "Invalid Username" });
+  if (!req.body.password?.length) return res.status(400).json({ message: "Invalid Password" });
+  const { username, password } : authInfo = req.body;
+
 };
 
+/**
+ * The handler for user login 
+ * 
+ * { username: string, password: string }
+ */
 export const login = (req: Request, res: Response) => {
-  res.send("Login");
+  if (!req.body) return res.status(400).json({ message: "missing request body" });
+  if (!req.body.username?.length) return res.status(400).json({ message: "Invalid Username" });
+  if (!req.body.password?.length) return res.status(400).json({ message: "Invalid Password" });
+  const { username, password } : authInfo = req.body;
 };
