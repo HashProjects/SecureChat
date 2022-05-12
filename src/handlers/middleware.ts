4fe0e2f2e6ade5f941d472d94ce0022123455dea
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-11 17:29:03 -0700

    Created middleware for http express traffic

---
+++ src/handlers/middleware.ts
@@ -0,0 +1,27 @@
+import logging from "../config/logging";
+import { Request, Response, NextFunction } from "express";
+
+const NAMESPACE = "middleware";
+
+export const logger = (req: Request, res: Response, next: NextFunction) => {
+  let ignore = /[(\.css)(\.js)(\.png)(\.ico)]/.test(req.url);
+  if (!ignore) logging.info(NAMESPACE, `${req.method} '${req.url}'`);
+
+  res.on("finish", () => {
+    if (!ignore)
+      logging.info(
+        NAMESPACE,
+        `${req.method} '${req.url}' -> ${res.statusCode} ${res.statusMessage} | ${req.socket.remoteAddress}`
+      );
+  });
+
+  next();
+};
+
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
+  logging.debug(NAMESPACE, res.statusCode.toString());
+
+  res.status(404).json({
+    error: "Not Found",
+  });
+}
