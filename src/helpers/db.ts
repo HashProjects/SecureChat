ea48ec3de46d4372b996bf262c98254cd054970c
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-11 21:42:08 -0700

    Added comments

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -2,9 +2,14 @@ import { Database } from "sqlite3";
 
 const db = new Database(`${__dirname}/../../database/main.db`);
 
+/**
+ * This is a async/await wrapper for sqlite3 functions
+ * @param {string} sql - the sql query
+ * @param {any} params
+ */
 export const query = (sql: string, params?: any) => {
   return new Promise((res, rej) => {
-    db.all(sql, params, (err, rows) => {
+    db.all(sql, params, (err: Error, rows: any[]) => {
       if (err) rej(err);
       res(rows);
     });
