6065c4388b7fae0440b36e7302a033c1a2f7aa78
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-11 17:28:16 -0700

    added rootDir to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import { LEVEL } from "node-color-log";
+import logging from "./logging";
+import path from "path";
 dotenv.config();
 
 const HOSTNAME = process.env.HOSTNAME ?? "localhost";
 const PORT: number = +(process.env.PORT ?? 8888);
-const LOG_LEVEL: LEVEL = process.env.LOG_LEVEL as LEVEL ?? "debug";
+const LOG_LEVEL: LEVEL = (process.env.LOG_LEVEL as LEVEL) ?? "debug";
+const ROOT_DIR = path.join(__dirname + "../../..")
 
 const SERVER = {
   host: HOSTNAME,
@@ -14,6 +17,7 @@ const SERVER = {
 const config = {
   server: SERVER,
   logLevel: LOG_LEVEL,
+  rootDir: ROOT_DIR,
 };
 
 export default config;
