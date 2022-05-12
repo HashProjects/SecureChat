61e50a4f9b658ce2304aee0486b89f9aca9bb043
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-11 17:29:47 -0700

    added async/await db helper functions

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,6 +1,8 @@
 import { Database } from "sqlite3";
 
-export const query = (db: Database, sql: string, params: any) => {
+const db = new Database(`${__dirname}/../../database/main.db`);
+
+export const query = (sql: string, params?: any) => {
   return new Promise((res, rej) => {
     db.all(sql, params, (err, rows) => {
       if (err) rej(err);
