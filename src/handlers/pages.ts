c8dcec9bcf80e84265ff43d25a0a430d551598cc
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-12 02:37:09 -0700

    Added auth to /chat

--- src/handlers/pages.ts
+++ src/handlers/pages.ts
@@ -1,16 +1,21 @@
 import { Request, Response } from "express";
 import config from "../config/config";
 import path from "path";
+import { auth } from "./auth";
+import logging from "../config/logging";
+
+const NAMESPACE = "pages";
 
 const dir = (file: string) => {
   return path.join(config.rootDir + "/public/" + file);
 };
 
 export const home = (req: Request, res: Response) => {
-  res.sendFile(dir("index.html"));
+  res.sendFile(dir("index.html"))
 };
 
 export const chat = (req: Request, res: Response) => {
+  if (!auth(req.cookies.auth)) return res.redirect("/login");
   res.sendFile(dir("chat.html"));
 };
 
