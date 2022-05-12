c4ede5271ff81c942f0d0c2f95a1e276b55914ec
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-12 02:37:00 -0700

    Finished Login, SIgnup, and JWTs

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -3,13 +3,30 @@ import logging from "../config/logging";
 import { query } from "../helpers/db";
 import User from "../models/User";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { Algorithm, JwtPayload } from "jsonwebtoken";
 import config from "../config/config";
 import { readFileSync } from "fs";
 
 const NAMESPACE = "auth";
-
 const SALT_ROUNDS = 10;
+
+/* Read JWT keys from files */
+const PRIVATE_KEY = readFileSync(config.jwt.private);
+const PUBLIC_KEY = readFileSync(config.jwt.public);
+
+const EXPIRATION = 60 * 60; // 1 hour
+
+const COOKIE_OPTIONS = {
+  maxAge: 1000 * EXPIRATION,
+};
+
+/**
+ * The Signing options for the JWT tokens
+ */
+const JWT_OPTIONS = {
+  expiresIn: EXPIRATION,
+  algorithm: "RS256" as Algorithm,
+};
 
 type authInfo = {
   username: string;
@@ -26,8 +43,26 @@ if (config.jwt.private.includes("default")) {
   );
 }
 
-const PRIVATE_KEY = readFileSync(config.jwt.private);
-const PUBLIC_KEY = readFileSync(config.jwt.public);
+/**
+ * Attempts to authenticate the provided jwt token
+ * @param {string} token - The JWT token
+ */
+export const auth = (token: string): JwtPayload | false => {
+  if (!token) return false;
+  logging.debug("authenticate", token);
+  try {
+    const data = jwt.verify(token, PUBLIC_KEY);
+    logging.debug("authenticate", "verify successful", data);
+    return data as JwtPayload;
+  } catch (e) {
+    logging.debug(NAMESPACE, "jwt.verify error", e);
+  }
+  return false;
+};
+
+const sign = (user: User): string => {
+  return jwt.sign({ username: user.name, id: user.id }, PRIVATE_KEY, JWT_OPTIONS);
+};
 
 /**
  * The handler for user registration.
@@ -46,34 +81,42 @@ export const register = async (req: Request, res: Response) => {
   const user = new User(username);
 
   /* Hash and Salt Password */
-  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  const hashedPassword = bcrypt.hashSync(password, SALT_ROUNDS);
 
   /* Insert into DB */
-  query("INSERT INTO Users (username, id, password) VALUES (?, ?, ?)", [
+  await query("INSERT INTO Users (username, id, password) VALUES (?, ?, ?)", [
     user.name,
     user.id,
     hashedPassword,
-  ])
-    /* If Successful */
-    .then(() => {
-      res.redirect("/chat");
-    })
-    /* If Error */
-    .catch((err) => {
-      /* Duplicate Username */
-      if (err.code === "SQLITE_CONSTRAINT") {
-        res.status(406).json({
-          message: "This username has already been taken",
-        });
-      } else {
-        /* Database failure */
-        logging.error(NAMESPACE, err);
-        res.status(500).json({
-          message: "Database Error",
-          error: err,
-        });
-      }
-    });
+  ]).catch((err) => {
+    /* Duplicate Username */
+    if (err.code === "SQLITE_CONSTRAINT") {
+      res.status(406).json({
+        message: "This username has already been taken",
+      });
+    } else {
+      /* Database failure */
+      logging.error(NAMESPACE, err);
+      res.status(500).json({
+        message: "Database Error",
+        error: err,
+      });
+    }
+  });
+
+  if (res.headersSent) return;
+
+  /* Query Successful */
+
+  /* Sign JWT */
+  const authCode = sign(user);
+
+  logging.debug(NAMESPACE, "Generated JWT Token", authCode);
+
+  /* Set Cookie */
+  res.cookie("auth", authCode, COOKIE_OPTIONS);
+
+  res.redirect("/chat");
 };
 
 /**
@@ -81,9 +124,53 @@ export const register = async (req: Request, res: Response) => {
  *
  * { username: string, password: string }
  */
-export const login = (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
   if (!req.body) return res.status(400).json({ message: "missing request body" });
   if (!req.body.username?.length) return res.status(400).json({ message: "Invalid Username" });
   if (!req.body.password?.length) return res.status(400).json({ message: "Invalid Password" });
   const { username, password }: authInfo = req.body;
+
+  /* Fetch hashedPassword from DB */
+  const rows = await query("SELECT password, id FROM Users WHERE username = ?", [username]).catch(
+    (err) => {
+      /* Database failure */
+      logging.error(NAMESPACE, err);
+      res.status(500).json({
+        message: "Database Error",
+        error: err,
+      });
+    }
+  );
+
+  if (res.headersSent) return;
+
+  if (!rows?.length || !rows[0].id || !rows[0].password) {
+    logging.debug(NAMESPACE, "Username does not exist");
+    return res.status(400).json({
+      message: "Username or password incorrect",
+    });
+  }
+
+  /* Query Successful */
+  const hashedPassword: string = rows[0].password;
+  const id: string = rows[0].id;
+  const user = new User(username, id);
+
+  /* Check Hashes */
+  if (!bcrypt.compareSync(password, hashedPassword)) {
+    logging.debug(NAMESPACE, "Password Incorrect");
+    return res.status(400).json({
+      message: "Username or password incorrect",
+    })
+  }
+
+  /* Sign JWT */
+  const authCode = sign(user);
+
+  logging.debug(NAMESPACE, "Generated JWT Token", authCode);
+
+  /* Set Cookie */
+  res.cookie("auth", authCode, COOKIE_OPTIONS);
+
+  res.redirect("/chat");
 };
