86edea55f1e4f1ef9a775192d4c3a0200bd16099
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-11 17:31:29 -0700

    Simplified server.ts to use handlers and routers
    Removed all request handling from server.ts to instead use the api router, main, router, http error middleware and socket handler

---
+++ src/server.ts
@@ -0,0 +1,60 @@
+import http from "http";
+import express from "express";
+import config from "./config/config";
+import logging from "./config/logging";
+import { Server } from "socket.io";
+import api from "./routes/api";
+import main from "./routes/main";
+import { connection } from "./handlers/sockets";
+import { logger, notFound } from "./handlers/middleware";
+
+const NAMESPACE = "index";
+const HOST = config.server.host;
+const PORT = config.server.port;
+
+export const app = express();
+export const server = http.createServer(app);
+export const io = new Server(server);
+
+/**
+ * Logs all requests and responses
+ */
+app.use(logger);
+
+/**
+ * Routes to the api router
+ */
+app.use("/api", api);
+
+/**
+ * Routes to the main router
+ */
+app.use("/", main);
+
+/**
+ * If route is not in routers, find file in public files
+ */
+app.use(express.static("public"));
+
+/**
+ * 404 Error handling
+ */
+app.use(notFound);
+
+/**
+ * Socket Connection
+ */
+io.on("connection", connection);
+
+/**
+ * Start listening on port
+ */
+server.listen(PORT, HOST, () => {
+  logging.info(NAMESPACE, `Server running on http://${HOST}:${PORT}`);
+});
+
+export default {
+  app,
+  server,
+  io,
+};
