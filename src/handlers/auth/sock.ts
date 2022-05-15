6e1975a5e42ce44ed4bf93e4561673f518eca849
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-15 12:30:45 -0700

    Uses Cookies
    Can use cookies now!

--- src/handlers/auth/sock.ts
+++ src/handlers/auth/sock.ts
@@ -2,21 +2,19 @@ import { auth } from "./helpers";
 import logging from "../../config/logging";
 import UserSocket from "../../models/UserSocket";
 import { Socket } from "socket.io";
+import { JSONCookie } from "cookie-parser";
 
 const NAMESPACE = "sockAuth";
 
+type AuthCookie = { auth: string | null };
+
 export const sockAuth = async (socket: Socket, next: any) => {
-  const unAuth = () => next(new Error("Unauthorized. Missing Token"));
-  const token = socket.handshake.auth?.token as string;
-
-  if (!token) return unAuth();
-
+  const cookies = JSONCookie(socket.request.headers?.cookie || "") as AuthCookie;
+  const token = cookies?.auth;
   const user = await auth(token);
-  if (!user) return unAuth();
+  if (!user) return next(new Error("Unauthorized. Missing Token"));
 
   (socket as UserSocket).user = user;
-
-  logging.debug(NAMESPACE, "auth successful");
 
   next();
 };
