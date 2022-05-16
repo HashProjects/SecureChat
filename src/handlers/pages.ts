29f2e11249b68c45918dbf94f81545ed469ad8aa
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-15 17:08:01 -0700

    moved dir to helpers, chat to chat handler

--- src/handlers/pages.ts
+++ src/handlers/pages.ts
@@ -1,22 +1,14 @@
 import { Request, Response } from "express";
-import config from "../config/config";
-import path from "path";
 import { auth } from "./auth/helpers";
 import logging from "../config/logging";
+import { dir } from "../helpers/misc";
 
 const NAMESPACE = "pages";
-
-const dir = (file: string) => {
-  return path.join(config.rootDir + "/public/" + file);
-};
 
 export const home = async (req: Request, res: Response) => {
   res.redirect("/chat");
 };
 
-export const chat = async (req: Request, res: Response) => {
-  res.sendFile(dir("chat.html"));
-};
 
 export const login = (req: Request, res: Response) => {
   res.sendFile(dir("login.html"));
