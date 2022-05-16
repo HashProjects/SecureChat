c736dcd938f7ef7aa6f9061db0be1a028f153133
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-15 18:47:14 -0700

    Added name to ChatRoom model

--- src/models/ChatRoom.ts
+++ src/models/ChatRoom.ts
@@ -1,11 +1,21 @@
 import { v4 as uuid } from "uuid";
 import User from "./User";
 
+const defaultName = (users: User[]) => {
+  let name = "";
+  for (let user of users) {
+    name += user.name + ", ";
+  }
+  return name.slice(0, -2);
+};
+
 class ChatRoom {
   public users: User[];
   public id: string;
-  constructor(users: User[], id?: string) {
+  public name: string;
+  constructor(users: User[], name?: string, id?: string) {
     this.users = users;
+    this.name = !name ? defaultName(users) : name;
     this.id = !id ? uuid() : id;
   }
 }
