76d614fd59ae9546170e136fb35d33734ce94908
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-05-15 17:08:47 -0700

    Routed endpoints for createRoom and RoomUsers

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import { authenticate, login, logout, register } from "../handlers/auth/http";
 import { Request, Response, NextFunction } from "express";
 import { Router } from "express";
 import logging from "../config/logging";
+import { createRoom, roomUsers } from "../handlers/rooms";
 
 const NAMESPACE = "API Router";
 
@@ -22,5 +23,8 @@ router.use(authenticate);
 /* Authenticated endpoints */
 
 router.post("/logout", logout as (req: Request, res: Response) => Promise<void>);
+router.post("/createRoom", createRoom as (req: Request, res: Response) => Promise<void>);
+router.post("/roomusers", roomUsers as (req: Request, res: Response) => Promise<void>);
+// router.post("/deleteRoom", logout as (req: Request, res: Response) => Promise<void>);
 
 export default router;
